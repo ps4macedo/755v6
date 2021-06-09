4e187c2f85820fc7a21a3b1dd87d77805983f9bd
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2021-06-09 13:48:04 -0300

    Add files via upload

--- ps4.js
+++ ps4.js
@@ -207,6 +207,9 @@ function toggle_payload(pld){
 	}else if(pld == "lamance132"){
 		document.getElementById("progress").innerHTML="Carregando o lamance132... Por favor, AGUARDE!!!";
 		preloadScripts(['preloader.js', 'lamance132.js', 'loader.js']);
+	}else if(pld == "toolbox"){
+		document.getElementById("progress").innerHTML="Carregando ToolBox... Por favor, AGUARDE!!!";
+		preloadScripts(['preloader.js', 'toolbox.js', 'loader.js']);
 	}else if(pld == "rif"){
 		document.getElementById("progress").innerHTML="Carregando RIF... Por favor, AGUARDE!!!";
 		preloadScripts(['preloader.js', 'rif.js', 'loader.js']);
